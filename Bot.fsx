7c3113177c55568b8131a64f100421067d467a8d
Hadrian Tang <hadrianwttang@outlook.com>
2022-05-24 22:23:25 +0800

    Update Bot.fsx

--- Bot.fsx
+++ Bot.fsx
@@ -48,7 +48,7 @@ open System.Threading.RateLimiting
         let emojisRateLimiter = new FixedWindowRateLimiter(FixedWindowRateLimiterOptions(1, QueueProcessingOrder.NewestFirst, 0, System.TimeSpan.FromSeconds 15.))
         let reactRateLimiter = new FixedWindowRateLimiter(FixedWindowRateLimiterOptions(1, QueueProcessingOrder.NewestFirst, 20, System.TimeSpan.FromSeconds 10.))
         let messageRateLimiter = new FixedWindowRateLimiter(FixedWindowRateLimiterOptions(1, QueueProcessingOrder.NewestFirst, 0, System.TimeSpan.FromSeconds 10.))
-        let reactCheckRateLimiter = new FixedWindowRateLimiter(FixedWindowRateLimiterOptions(1, QueueProcessingOrder.NewestFirst, 20, System.TimeSpan.FromSeconds 2.))
+        let reactCheckRateLimiter = new FixedWindowRateLimiter(FixedWindowRateLimiterOptions(1, QueueProcessingOrder.NewestFirst, 10, System.TimeSpan.FromSeconds 2.))
         let mutable emojis = Array.empty :> System.Collections.Generic.IReadOnlyList<DiscordEmoji>
         let mutable messageReceivedHandler = fun _ _ -> ()
         let mutable reactionAddedHandler = fun _ _ -> ()
